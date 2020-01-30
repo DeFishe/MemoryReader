687c3567bc4f1c14f230edfb382278000e746abe
Darian Fisher <darian.edward.fisher@outlook.com>
2020-01-29 19:53:54 -0500

    Update MemoryReader.cpp
    Added execption handling for ReadProcessMemory attempt. Added prompts for user input to determine PID and memory address.

--- MemoryReader/MemoryReader.cpp
+++ MemoryReader/MemoryReader.cpp
@@ -7,18 +7,34 @@ using namespace std;
 int main()
 {
     int intRead = 0;
+    int PID = 0;
+    uintptr_t memoryAddress;
+    bool RPMSuccess;
 
-    HANDLE fProccess = OpenProcess(PROCESS_ALL_ACCESS, false, 12028); //Last argument is PID. ALWAYS GET CURRENT PID (PROCESS ID).
+    cout << "Write the PID of the application you want to read: ";
+    cin >> PID;
+
+    HANDLE fProccess = OpenProcess(PROCESS_ALL_ACCESS, false, PID); //Last argument is PID. ALWAYS GET CURRENT PID (PROCESS ID).
     if (fProccess == NULL)
     {
         cout << "OpenProcess method failed with the following error: " << GetLastError() << endl;
         getchar();
         return EXIT_FAILURE;
     }
-    ReadProcessMemory(fProccess, (LPCVOID)0x008FFD18, &intRead, sizeof(int), NULL);
+
+    cout << "Write the memory address that you want to read. Must hold an integer: 0x";
+    cin >> hex >> memoryAddress;
+
+    RPMSuccess = ReadProcessMemory(fProccess, (LPCVOID)memoryAddress, &intRead, sizeof(int), NULL);
+    if (RPMSuccess == NULL)
+    {
+        cout << "ReadProcessMemory method failed with the following error: " << GetLastError() << endl;
+        getchar();
+        return EXIT_FAILURE;
+    }
     
-    cout << "The memory read stored the integer " << intRead << endl;
-    cout << "PRESS ANY KEY TO EXIT.";
+    cout << "\nThe memory read held the integer " << intRead << endl;
+    cout << "\nPRESS ANY KEY TO EXIT.";
     getchar();
 
     return EXIT_SUCCESS;
